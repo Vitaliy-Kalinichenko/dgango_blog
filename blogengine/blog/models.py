7733f2d2458cc157deb1cf1c76ed6263cf0d3d25
Vitaliy-Kalinichenko <vitalya3007@gmail.com>
2021-05-27 17:51:59 +0300

    Create model, first migration, posts_detail

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls.base import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('posts_detail', kwargs={'slug': self.slug})  # reverse (ф-я url в шаблонах) генерирует ссылку,
+        # передаем ей назв. юрл шаблона и словарь в качестве ключя - поле по которому мы проводим идентификацию объекта
+
+    def __str__(self):
+        return self.title
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from .models import *
+
 
 def posts_list(request):
-    n = ['Vitaliy', 'Oleg', 'Tanya', 'Ksu']
+    posts = Post.objects.all()
     return render(request, 'blog/index.html',
-                  context={'names': n})  # ключи словаря контекст, будут использованы в шаблоне
+                  context={'posts': posts})  # ключи словаря контекст, будут использованы в шаблоне
+
+
+def posts_detail(request, slug):  # slug - то что приходит из urlpatterns
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/posts_detail.html', context={'post': post})
